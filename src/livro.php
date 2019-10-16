e71dbc1ad8a73aa52b8c22440b6728c5880f66ef
Ketlyn Sena <ketlynsena@outlook.com>
2019-10-15 21:31:36 -0300

    Alterações para incluir código do autor e da editora

--- src/livro.php
+++ src/livro.php
@@ -8,15 +8,25 @@ class Livro{
     private $Faixa_Etaria;
     private $Descricao;
 
-    function __construct($ISBN, $Titulo, $Ano, $Faixa_Etaria, $Descricao){
+    private $Cod_Autor;
+    private $Cod_Editora;
+    private $Cod_Categoria;
+
+    function __construct($ISBN, $Titulo, $Ano, $Faixa_Etaria, $Descricao, $Cod_Autor, $Cod_Editora){
 
         $this->ISBN = $ISBN;
         $this->Titulo = $Titulo;
         $this->Ano = $Ano;
         $this->Faixa_Etaria = $Faixa_Etaria;
         $this->Descricao = $Descricao;
+        $this->Cod_Autor = $Cod_Autor;
+        $this->Cod_Editora = $Cod_Editora;
 
         #print "Criando o objeto ".$this->Titulo."\n";
+    }
+
+    function setCod_Categoria($Cod_Categoria){
+        $this->Cod_Categoria[] = $Cod_Categoria;
     }
 
     function getISBN(){
@@ -37,6 +47,18 @@ class Livro{
 
     function getDescricao(){
         return $this->Descricao;
+    }
+
+    function getCod_Autor(){
+        return $this->Cod_Autor;
+    }
+
+    function getCod_Editora(){
+        return $this->Cod_Editora;
+    }
+
+    function getCod_Categoria(){
+        return $this->Cod_Categoria;
     }
 
     function ImprimeLivro(){
