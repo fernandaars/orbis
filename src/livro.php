a8fe42385d529947d69698fd8e6546f394205572
Ketlyn Sena <ketlynsena@outlook.com>
2019-10-17 22:29:59 -0300

    Modificação para receber link do pdf

--- src/livro.php
+++ src/livro.php
@@ -7,20 +7,24 @@ class Livro{
     private $Ano;
     private $Faixa_Etaria;
     private $Descricao;
+    private $PDF;
 
     private $Cod_Autor;
     private $Cod_Editora;
     private $Cod_Categoria;
 
-    function __construct($ISBN, $Titulo, $Ano, $Faixa_Etaria, $Descricao, $Cod_Autor, $Cod_Editora){
+    function __construct($ISBN, $Titulo, $Ano, $Faixa_Etaria, $Descricao, $PDF, $Cod_Autor, $Cod_Editora){
 
         $this->ISBN = $ISBN;
         $this->Titulo = $Titulo;
         $this->Ano = $Ano;
         $this->Faixa_Etaria = $Faixa_Etaria;
         $this->Descricao = $Descricao;
+        $this->PDF = $PDF;
+
         $this->Cod_Autor = $Cod_Autor;
         $this->Cod_Editora = $Cod_Editora;
+        
 
         #print "Criando o objeto ".$this->Titulo."\n";
     }
@@ -47,6 +51,10 @@ class Livro{
 
     function getDescricao(){
         return $this->Descricao;
+    }
+
+    function getPDF(){
+        return $this->PDF;
     }
 
     function getCod_Autor(){
