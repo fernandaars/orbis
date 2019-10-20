d944b0a2c24027fa32f358fed614372998b97adb
Ketlyn Sena <ketlynsena@outlook.com>
2019-10-20 07:56:35 -0300

    Classe usuario com consultas ao banco

--- src/usuario.php
+++ src/usuario.php
@@ -14,7 +14,7 @@ class Usuario{
 
     private $Conexao;
 
-    function __construct($Nome, $Email, $Senha, $Idade, $Favoritos, $Avaliacoes){
+    function __construct($Nome, $Email, $Senha, $Foto, $Idade, $Favoritos, $Avaliacoes){
         $this->Nome = $Nome;
         $this->Email = $Email;
         $this->Senha = $Senha;
@@ -23,7 +23,7 @@ class Usuario{
         $this->Favoritos = $Favoritos;
         $this->Avaliacoes = $Avaliacoes;
 
-        include_once "conexao_banco.php"; //importa conexão $conn
+        include "conexao_banco.php"; //importa conexão $conn
         $this->Conexao = $conn;
 
         include_once "base.php"; //Importa Base
@@ -72,23 +72,44 @@ class Usuario{
         $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
         if ($Livro != NULL){
             $this->Favoritos[] = $Livro->getISBN();
-            #$this->insertFavoritos($Livro->getISBN());
+            $this->insertFavoritos($Livro->getISBN());
             return TRUE;
         }
         else
             return FALSE;
     }
 
+    function insertFavoritos($ISBN){
+        $stmt = $this->Conexao->prepare('INSERT INTO usuarioListaFav(isbn, email) VALUES ( :ISBN, :Email)');
+        $stmt->bindParam(':ISBN', $ISBN, PDO::PARAM_INT);
+        $stmt->bindParam(':Email', $this->Email, PDO::PARAM_STR);
+        $stmt->execute();
+        #echo " adicionou: ";
+        #print($ISBN);
+    }
 
-    function removeFavoritos($Nome_livro){
+
+    function removeFavoritos($Nome_Livro){
         $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
-        $key = array_search($Livro->getISBN(), $this->Favoritos);
-        if(isset($this->Favoritos[$Livro->getISBN()])) {
-            unset($this->Favoritos[$Livro->getISBN()]);
+        if ($Livro != NULL){
+            $key = array_search($Livro->getISBN(), $this->Favoritos);
+            unset($this->Favoritos[$key]);
+            $this->deleteFavoritos($Livro->getISBN());
+            
         }
     }
 
-    function inFavoritos($Nome_livro){
+    function deleteFavoritos($ISBN){
+        $query = 'DELETE FROM usuarioListaFav WHERE isbn = :ISBN AND email = :Email';
+        $stmt = $this->Conexao->prepare('DELETE FROM usuarioListaFav WHERE isbn = :ISBN AND email = :Email');
+        $stmt->bindParam(':ISBN', $ISBN, PDO::PARAM_INT);
+        $stmt->bindParam(':Email', $this->Email, PDO::PARAM_STR);
+        $stmt->execute();
+        #echo " excluiu: ";
+        #print($ISBN);
+    }
+
+    function inFavoritos($Nome_Livro){
         $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
         return in_array($Livro->getISBN(), $this->Favoritos);
     }
@@ -97,18 +118,49 @@ class Usuario{
         $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
         if ($Livro != NULL){
             $this->Avaliacoes[$Livro->getISBN()] = $Nota;
-            #$this->insertAvaliacoes($Livro->getISBN(), $Nota);
+            $this->insertAvaliacoes((int)$Livro->getISBN(), $Nota);
             return TRUE;
         }
         else
             return FALSE;
     }
 
-    function removeAvaliacoes($Nome_livro, $Nota){
-        $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
-        if(isset($this->Avaliacoes[$Livro->getISBN()])) {
-            unset($this->Avaliacoes[$Livro->getISBN()]);
+    function insertAvaliacoes($ISBN, $Nota){
+        $stmt = $this->Conexao->prepare('INSERT INTO usuarioAvaliacao(email, isbn ,nota) VALUES (:Email, :ISBN, :Nota)');
+        $params = array(
+            'ISBN' => $ISBN, 
+            'Nota' => $Nota,
+            'Email'=> $this->Email,
+        );
+        #print_r($params);
+        if ( ! $stmt->execute ($params )){
+            print_r( $stmt->errorInfo ());
         }
+        echo " adicionou: ";
+        print($ISBN);
+    }
+
+    function removeAvaliacoes($Nome_Livro){
+        $Livro = $this->Base_Usuario->retornaLivroTitulo($Nome_Livro);
+        if ($Livro != NULL){
+            unset($this->Avaliacoes[$Livro->getISBN()]);
+            $this->deleteAvaliacoes($Livro->getISBN());
+        }
+    }
+
+    function deleteAvaliacoes($ISBN){
+        $stmt = $this->Conexao->prepare('DELETE FROM usuarioAvaliacao WHERE isbn = :ISBN AND email = :Email');
+        $stmt->bindParam(':ISBN', $ISBN, PDO::PARAM_INT);
+        $stmt->bindParam(':Email', $this->Email, PDO::PARAM_STR);
+        $params = array(
+            'ISBN' => $ISBN,
+            'Email'=> $this->Email,
+        );
+        if(! $stmt->execute($params)){
+            print_r( $stmt->errorInfo());
+        }
+        #echo " excluiu: ";
+        #print($ISBN);
     }
 
     function returnAvaliacaoLivro($Nome_livro){
@@ -119,32 +171,16 @@ class Usuario{
         else 
             return FALSE;
     }
-
-    function insertFavoritos($ISBN){
-        $query = 'INSERT INTO usuarioListaFav(isbn, email) VALUES ( :ISBN, :Email)';
-        $stmt = $this->Conexao->prepare('INSERT INTO usuarioListaFav(isbn, email) VALUES ( :ISBN, :Email)');
-        $stmt->bindParam(':ISBN', $ISBN, PDO::PARAM_INT);
-        $stmt->bindParam(':Email', $this->Email, PDO::PARAM_STR);
-        $stmt->execute();
-        print($ISBN);
-    }
-
-    function insertAvaliacoes($ISBN, $Nota){
-        $query = 'INSERT INTO usuarioAvaliacao(isbn, email, nota) VALUES ( :ISBN, :Email, :Nota)';
-        $stmt = $this->Conexao->prepare('INSERT INTO usuarioAvaliacao(isbn, email, nota) VALUES ( :ISBN, :Email, :Nota)');
-        $stmt->bindParam(':ISBN', $ISBN, ':Nota', $Nota, PDO::PARAM_INT);
-        $stmt->bindParam(':Email', $this->Email, PDO::PARAM_STR);
-        $stmt->execute();
-        print($ISBN);
-    }
     
-    function __destruct(){
-        
+    function __destruct(){  
     }
 
 }
 
-$u = new Usuario("cristina", "cristina@gmail.com", "123", 9, NULL, NULL);
-$u->addFavoritos("O Menino Maluquinho");
+#$u = new Usuario("cristina", "cristina@gmail.com", "123", NULL, 9, NULL, NULL);
+#$u->addFavoritos("O Menino Maluquinho");
+#$u->removeFavoritos("O Menino Maluquinho");
+#$u->addAvaliacoes("O Menino Maluquinho", 5);
+#$u->removeAvaliacoes("O Menino Maluquinho");
 
 ?>
